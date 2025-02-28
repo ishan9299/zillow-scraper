ac67e6f6813eaefcf070a8d66eaa21638a1ec3d5
Ishan <ishan365.ia@gmail.com>
2025-02-28 10:52:33 +0530

    update actions

--- .github/workflows/generate_actions.py
+++ .github/workflows/generate_actions.py
@@ -6,7 +6,7 @@ name: Scrape {arg}
 on:
   workflow_dispatch:
   schedule:
-    - cron: "0 5 * * *"
+    - cron: "30 5 * * *"
 
 jobs:
 """
@@ -57,57 +57,56 @@ for i, arg in enumerate(args):
 
           echo "git push"
           git push origin master
-
     """.rstrip()
 
 # Generate final job
-job_names = ", ".join([f"actions_{i}" for i in range(0, len(args))])
-content += f"""
-
-  final-job:
-    needs: actions_256
-    runs-on: ubuntu-latest
-    steps:
-      - name: checkout repo content
-        uses: actions/checkout@v2
-
-      - name: setup python
-        uses: actions/setup-python@v4
-        with:
-          python-version: '3.13'
-
-      - name: install python packages
-        run: |
-          python -m pip install --upgrade pip
-          pip install pandas
-
-      - name: execute python script
-        run: python merge_csvs.py
-
-      - name: setup ssh
-        run: |
-          mkdir -p ~/.ssh
-          echo "${{ secrets.DEPLOY_KEY }}" > ~/.ssh/id_rsa
-          chmod 600 ~/.ssh/id_rsa
-          ssh-keyscan github.com >> ~/.ssh/known_hosts
-
-      - name: git
-        run: |
-          git config --global user.email "action@github.com"
-          git config --global user.name "GitHub Action"
-          git remote set-url origin git@github.com:ishan9299/zillow-scraper.git
-
-          echo "git commit"
-          git add .
-          git commit -m "Automated scrape for file 0 - $(date +%F)"
-
-          echo "git pull"
-          git config pull.rebase true
-          git pull origin
-
-          echo "git push"
-          git push origin master
-""".rstrip()
+# job_names = ", ".join([f"actions_{i}" for i in range(0, len(args))])
+# content += f"""
+#
+#   final-job:
+#     needs: actions_256
+#     runs-on: ubuntu-latest
+#     steps:
+#       - name: checkout repo content
+#         uses: actions/checkout@v2
+#
+#       - name: setup python
+#         uses: actions/setup-python@v4
+#         with:
+#           python-version: '3.13'
+#
+#       - name: install python packages
+#         run: |
+#           python -m pip install --upgrade pip
+#           pip install pandas
+#
+#       - name: execute python script
+#         run: python merge_csvs.py
+#
+#       - name: setup ssh
+#         run: |
+#           mkdir -p ~/.ssh
+#           echo "${{ secrets.DEPLOY_KEY }}" > ~/.ssh/id_rsa
+#           chmod 600 ~/.ssh/id_rsa
+#           ssh-keyscan github.com >> ~/.ssh/known_hosts
+#
+#       - name: git
+#         run: |
+#           git config --global user.email "action@github.com"
+#           git config --global user.name "GitHub Action"
+#           git remote set-url origin git@github.com:ishan9299/zillow-scraper.git
+#
+#           echo "git commit"
+#           git add .
+#           git commit -m "Automated scrape for file 0 - $(date +%F)"
+#
+#           echo "git pull"
+#           git config pull.rebase true
+#           git pull origin
+#
+#           echo "git push"
+#           git push origin master
+# """.rstrip()
 
 with open("actions.yml", "w") as action_file:
     action_file.write(f"{content}")
